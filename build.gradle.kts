c444a8d53fc5f32fea909ccde248e901b061b194
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2023-02-08 10:35:11 +0900

    Word Book Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.8"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "acc"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.3.0")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/acc/wordbook/WordbookApplicationTests.java
@@ -0,0 +1,13 @@
+package acc.wordbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
