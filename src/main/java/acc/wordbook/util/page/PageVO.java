ae3b222e3375496e913af07fb4419202360ac5b5
DESKTOP-2A54VHJ\user <jackal132@naver.com>
2023-02-13 09:41:29 +0900

    MyPage 페이징 오류 수정

--- src/main/java/acc/wordbook/util/page/PageVO.java
+++ src/main/java/acc/wordbook/util/page/PageVO.java
@@ -12,13 +12,20 @@ public class PageVO {
     //사용자가 선택한 페이지 정보를 담을 변수.
     private int pageNum;
     private int countPerPage;
+    private int offset;
 
     // 검색에 필요한 데이터
     private String keyword;
     private String searchField;
 
+
     public PageVO(){
         this.pageNum = 1;
         this.countPerPage = 10;
+    }
+
+    public int getOffset(){
+        this.offset = (this.pageNum -1) * countPerPage;
+        return this.offset;
     }
 }
